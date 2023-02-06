d06522016d97e9be26a81b5042b1c245586a55fd
irsan mansyur <irsan00mansyur@gmail.com>
2023-02-07 01:47:55 +0700

    update

--- src/components/tool-tip/index.tsx
+++ src/components/tool-tip/index.tsx
@@ -12,15 +12,15 @@ export default function Tooltip({ text, show, children }: Props) {
       className="group relative inline-block"
       onMouseEnter={({ clientX }) => {
         if (!toolTipRef.current || !toolTipParentRef.current) return;
-        const { left } = toolTipParentRef.current.getBoundingClientRect();
-        toolTipRef.current.style.left = clientX - left + "px";
+        // const { left } = toolTipParentRef.current.getBoundingClientRect();
+        // toolTipRef.current.style.left = clientX - left + "px";
       }}
     >
       {children}
       {show && text && (
         <span
           ref={toolTipRef}
-          className={"invisible group-hover:visible p-1 rounded absolute top-full mt-1 whitespace-nowrap bg-slate-700 text-white text-xs"}
+          className={`invisible group-hover:visible p-1 rounded absolute top-full mt-1 left-1/2 -translate-x-1/2 sm:whitespace-nowrap bg-slate-700 text-white text-xs`}
           dangerouslySetInnerHTML={{ __html: text }}
         />
       )}
